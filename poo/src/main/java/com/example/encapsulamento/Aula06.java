462a22238103f1fef38228071df5a6c48e9c92e9
keillaarr <keillaarruda9@gmail.com>
2024-06-21 11:23:10 -0300

    Incluindo Encapsulamento

---
+++ poo/src/main/java/com/example/encapsulamento/Aula06.java
@@ -0,0 +1,10 @@
+package com.example.encapsulamento;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto(); 
+        c.ligar();
+        c.ligarMudo();
+        c.abrirMenu();
+    }
+}
--- poo/src/main/java/com/example/metodosEspeciais/Conta.java
+++ poo/src/main/java/com/example/metodosEspeciais/Conta.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.metodosEspeciais;
 public class Conta {
     private static String tipoDeConta = null;
     private String nomeCliente; 
